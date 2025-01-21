de23d2a65420d0cd4b1a249406fe91ac6189e497
hyperneutrino <hyperneutrino15@gmail.com>
2025-01-21 13:07:04 -0500

    implement new contributors page

--- app/contributors/page.tsx
+++ app/contributors/page.tsx
@@ -1,92 +1,132 @@
 "use client";
 
-import { BottomNav } from "@/components/BottomNav";
 import { Header } from "@/components/Header";
-import { Panel } from "@/components/Panel";
-import { SectionHeader } from "@/components/SectionHeader";
-import { CPWebDevs, communityReps, designers } from "@/data/contributors";
-import { pageRoutes } from "@/data/routes";
-
-interface ContributorProfile {
-  name: string;
-  link: string;
-}
-
-interface ContributorGroupProps {
-  group: Array<ContributorProfile>;
-}
-
-function ContributorGroup({ group }: ContributorGroupProps) {
-  return (
-    <ul>
-      {group
-        .sort((a, b) => a.name.localeCompare(b.name))
-        .map((d, idx) => {
-          return (
-            <li key={idx}>
-              {d.link ? (
-                <a href={d.link} target="_blank" rel="noreferrer">
-                  {d.name}
-                </a>
-              ) : (
-                <a href="#" onClick={(e) => e.preventDefault()}>
-                  {d.name}
-                </a>
-              )}
-            </li>
-          );
-        })}
-    </ul>
-  );
-}
+import PageHeader from "@/components/PageHeader";
+import { classProfileWebDevelopers, communityRepresentatives, designers, pastContributors } from "@/data/contributors";
+import { basePath } from "@/utils/getBasePath";
+import { useWindowDimensions } from "@/utils/getWindowDimensions";
 
 export default function Contributors() {
+  const pageWidth = useWindowDimensions().width;
+
   return (
     <div className="flex flex-col items-center">
       <Header />
-      <SectionHeader
-        title="Contributors"
-        subtitle="Huge thanks to all CSC members who have contributed to creating the first ever uWaterloo CS class profile!"
-      />
-      <Panel>
-        <p>
-          The 2023 CS Class Profile was completed by members of the UW Computer Science Club. Specifically, several current and past members (as of this
-          writing) of the Community Representatives, Designers, Class Profile Web Developers, and Systems Committee put lots of time into making it what it is.
-          Please contact <a href="mailto: exec@csclub.uwaterloo.ca">exec@csclub.uwaterloo.ca</a> for specific concerns for the CS Class Profile, but the
-          specific contributors include the following:
-        </p>
-        <ul>
-          <li>
-            Community Representatives
-            <ContributorGroup group={communityReps} />
-          </li>
-          <li>
-            Designers
-            <ContributorGroup group={designers} />
-          </li>
-          <li>
-            Class Profile Web Developers
-            <ContributorGroup group={CPWebDevs} />
-          </li>
-          {/* <li>
-            Systems Committee
-            <ContributorGroup group={sysCom} />
-          </li> */}
-        </ul>
-        <p>
-          Furthermore, special thanks to former CSC President and Vice President,{" "}
-          <a href="https://www.linkedin.com/in/kallentu/" target="_blank" rel="noreferrer">
-            Kallen Tu
-          </a>{" "}
-          and{" "}
-          <a href=" https://www.linkedin.com/in/gordonle/" target="_blank" rel="noreferrer">
-            Gordon Le
-          </a>
-          , for kickstarting the existence of the CS Class Profile and for providing feedback! Note there may be other anonymous contributors on any of the
-          teams or committees, but their names are not listed here.
-        </p>
-      </Panel>
-      <BottomNav leftPage={pageRoutes.personal} rightPage={pageRoutes.home}></BottomNav>
+      <PageHeader name="contributors" alt="Contributors" />
+
+      <div
+        className="w-full flex flex-col items-center gap-32"
+        style={{
+          backgroundImage: `url(${JSON.stringify(basePath + "/images/contributors-main.png")})`,
+          backgroundSize: "100% auto",
+          backgroundPosition: "center top",
+          minHeight: (pageWidth / 6912) * 16769,
+        }}>
+        <div className="w-[calc(min(80%,800px))] rounded-lg p-8" style={{ backgroundImage: "linear-gradient(120deg, #f8f9fd, #b5c1ec)" }}>
+          <span className="font-bold text-dark-blue md:text-lg xl:text-xl">
+            Congratulations, Class of 2024! We can't wait to see the amazing things you'll achieve.
+          </span>
+          <div className="flex justify-end">
+            <span className="font-bold text-black/50 text-sm md:text-md xl:text-lg">&mdash; With love, CSC &#x1F499;</span>
+          </div>
+        </div>
+        <div className="w-[calc(min(90%,1200px))] flex justify-end">
+          <div className="w-[calc(min(80%,600px))] rounded-lg p-8" style={{ backgroundImage: "linear-gradient(150deg, #443996a0, #ffcad0a0)" }}>
+            <span className="text-white text-sm md:text-md xl:text-lg">
+              The 2024 CS Class Profile was created through the hard work of past and present members of the UW Computer Science Club's Community
+              Representatives, Designers, Class Profile Web Developers, and Systems Committee. Please contact{" "}
+              <a href="mailto:exec@csclub.uwaterloo.ca" className="text-white underline">
+                exec@csclub.uwaterloo.ca
+              </a>{" "}
+              for specific concerns for the CS Class Profile.
+            </span>
+          </div>
+        </div>
+        <div className="w-[calc(min(90%,1200px))] flex justify-start">
+          <div className="w-[calc(min(80%,600px))] rounded-lg p-8" style={{ backgroundImage: "linear-gradient(150deg, #443996a0, #ffcad0a0)" }}>
+            <span className="text-white text-sm md:text-md xl:text-lg">
+              Special thanks to former CSC President and Vice-President{" "}
+              <a href="https://linkedin.com/in/kallentu" target="_blank" className="text-white underline">
+                Kallen Tu
+              </a>{" "}
+              and{" "}
+              <a href="https://linkedin.com/in/gordonle" target="_blank" className="text-white underline">
+                Gordon Le
+              </a>{" "}
+              for kickstarting the existence of the CS Class Profile! In addition to the names listed below, there may be additional anonymous contributors and
+              those who have provided feedback.
+            </span>
+          </div>
+        </div>
+        <div className="w-full flex justify-center">
+          <div className="w-[calc(min(80%,800px))] rounded-lg p-8" style={{ backgroundImage: "linear-gradient(150deg, #443996a0, #ffcad0a0)" }}>
+            <div className="flex flex-col gap-4">
+              <h3 className="text-transparent bg-clip-text" style={{ backgroundImage: "linear-gradient(172deg, #ffffff, #ffc7f3)" }}>
+                Class Profile Web Developers
+              </h3>
+              <ul className="list-disc list-inside">
+                {classProfileWebDevelopers.map(({ name, link, lead }) => (
+                  <li key={link}>
+                    <a href={link} target="_blank" className="text-white underline">
+                      {lead ? <b>{name}</b> : name}
+                    </a>
+                    {lead ? <span className="opacity-50"> &mdash; Team Lead</span> : null}
+                  </li>
+                ))}
+              </ul>
+              <h3 className="text-transparent bg-clip-text" style={{ backgroundImage: "linear-gradient(172deg, #ffffff, #ffda7a)" }}>
+                Designers
+              </h3>
+              <ul className="list-disc list-inside">
+                {designers.map(({ name, link, lead }) => (
+                  <li key={link}>
+                    <a href={link} target="_blank" className="text-white underline">
+                      {lead ? <b>{name}</b> : name}
+                    </a>
+                    {lead ? <span className="opacity-50"> &mdash; Team Lead</span> : null}
+                  </li>
+                ))}
+              </ul>
+              <h3 className="text-transparent bg-clip-text" style={{ backgroundImage: "linear-gradient(172deg, #ffffff, #a072eb)" }}>
+                Community Reps
+              </h3>
+              <ul className="list-disc list-inside">
+                {communityRepresentatives.map(({ name, link, lead }) => (
+                  <li key={link}>
+                    <a href={link} target="_blank" className="text-white underline">
+                      {lead ? <b>{name}</b> : name}
+                    </a>
+                    {lead ? <span className="opacity-50"> &mdash; Team Lead</span> : null}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="w-full flex justify-center">
+          <div className="w-[calc(min(80%,800px))] rounded-lg p-8" style={{ backgroundImage: "linear-gradient(150deg, #443996a0, #ffcad0a0)" }}>
+            <div className="flex flex-col gap-4">
+              <h3 className="text-transparent bg-clip-text" style={{ backgroundImage: "linear-gradient(172deg, #ffffff, #a072eb)" }}>
+                Past Contributors
+              </h3>
+              <span className="text-white">
+                Our thanks go out to previous contributors as well who built the 2022 and 2023 versions of the Class Profile on which the vast majority of this
+                version was built. A lot of work is shared year to year, so we want to express our gratitude to the previous teams as well.
+              </span>
+              <ul className="list-disc list-inside">
+                {pastContributors.map(({ name, link, role }) => (
+                  <li key={link}>
+                    <a href={link} target="_blank" className="text-white underline">
+                      {name}
+                    </a>{" "}
+                    ({role})
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- data/contributors.ts
+++ data/contributors.ts
@@ -1,32 +1,65 @@
-export const communityReps = [
-  { name: "Kim Guo", link: "https://www.linkedin.com/in/kbrqin/" },
-  { name: "Imane Yacoubi", link: "https://www.linkedin.com/in/iyacoubi" },
-  { name: "Ashnoor Randhawa", link: "https://www.linkedin.com/in/ashnoor-kaur/" },
-  { name: "Mayank Mehra", link: "https://www.linkedin.com/in/mayank808/" },
-  { name: "Katherine Liu", link: "https://www.linkedin.com/in/katherine-liu-59b30324a/" },
-  { name: "Ivy Lei", link: "https://www.linkedin.com/in/ivy-lei/" },
-  { name: "Kevin Cui", link: "https://www.linkedin.com/in/kevincui1" },
-];
-
-export const designers = [
-  { name: "Kim Guo", link: "https://www.linkedin.com/in/kbrqin/" },
-  { name: "Cadey Chen", link: "https://www.linkedin.com/in/cadey-chen/" },
-  { name: "Ashlyn Wu", link: "https://www.linkedin.com/in/ashlynwu/" },
-  { name: "Roni Wu", link: "https://www.linkedin.com/in/roni-wu-908900140/" },
-  { name: "Jessica Chan", link: "https://www.linkedin.com/in/jessica-r-chan/" },
-  { name: "Linda Wang", link: "https://www.linkedin.com/in/linda-y-wang/" },
-  { name: "Kelly Qu", link: "https://www.linkedin.com/in/kellyqu/" },
-  { name: "Anny Wei", link: "https://www.linkedin.com/in/annyzwei/" },
-  { name: "Lucy Qi", link: "https://www.linkedin.com/in/lucyqqi/" },
-  { name: "Lila Hoang", link: "https://www.linkedin.com/in/lila-hoang-a13851263/" },
-  { name: "Hailey Chan", link: "https://www.linkedin.com/in/chan-hailey/" },
-];
-
-export const CPWebDevs = [
-  { name: "Alexander Liao", link: "https://www.linkedin.com/in/alexander-liao/" },
-  { name: "Kushal Goel", link: "https://www.linkedin.com/in/kushal-goel/" },
+export const classProfileWebDevelopers: { name: string; link: string; lead?: boolean }[] = [
+  { name: "Iris Liao", link: "https://www.hyperneutrino.xyz", lead: true },
   { name: "Cathie Yan", link: "https://www.linkedin.com/in/cathie-yan/" },
-  { name: "Ryan Zhou", link: "https://www.linkedin.com/in/ryan-zhou-498b94285/" },
-  { name: "Emily Huang", link: "https://www.linkedin.com/in/emilyhuxng/" },
-  { name: "Franklin Ramirez", link: "https://www.linkedin.com/in/franklin-ramirez611/" },
+  { name: "Cindy Li", link: "https://www.linkedin.com/in/cindehaa/" },
+  { name: "Om Ogale", link: "https://omogale.vercel.app/" },
+  { name: "Vivek Manoj", link: "https://www.linkedin.com/in/vivekjudemanoj/" },
+  { name: "Yiyan Li", link: "https://yiyanhuang.netlify.app/" },
+];
+
+export const designers: { name: string; link: string; lead?: boolean }[] = [
+  { name: "Kim Guo", link: "https://linkedin.com/in/kbrqin/", lead: true },
+  { name: "Anny Wei", link: "https://linkedin.com/in/annyzwei/" },
+  { name: "Areeb Rasul", link: "https://www.linkedin.com/in/areeb-rasul/" },
+  { name: "Cindy Li", link: "https://www.linkedin.com/in/cindehaa/" },
+  { name: "Grace Lee", link: "https://www.linkedin.com/in/jaeeungracelee/" },
+  { name: "Jia Huang", link: "https://www.linkedin.com/in/yi-jia-huang/" },
+  { name: "Lila Hoang", link: "https://www.linkedin.com/in/lila-hoang/" },
+  { name: "Yujin Bae", link: "https://www.linkedin.com/in/yujin-bae-030445270/" },
+];
+
+export const communityRepresentatives: { name: string; link: string; lead?: boolean }[] = [
+  { name: "Yash Choudhary", link: "https://yashchoudharyx.github.io/Portfolio/", lead: true },
+  { name: "Lindsay Xie", link: "https://www.linkedin.com/in/lindsayxie/", lead: true },
+  { name: "Alia Cai", link: "https://www.linkedin.com/in/alia-cai-9795752a5/" },
+  { name: "Ethan Kwon", link: "https://www.linkedin.com/in/ethan-kwon/" },
+  { name: "Reina Kuan", link: "#" },
+];
+
+export const pastContributors: { name: string; link: string; role: string }[] = [
+  { name: "Aaryan Shroff", link: "https://www.linkedin.com/in/aaryan-shroff", role: "Design" },
+  { name: "Amy Luo", link: "https://www.linkedin.com/in/amytluo/", role: "Reps" },
+  { name: "Amy Wang", link: "#", role: "Web Dev" },
+  { name: "Ashlyn Wu", link: "https://linkedin.com/in/ashlynwu/", role: "Design" },
+  { name: "Ashnoor Randhawa", link: "https://www.linkedin.com/in/ashnoor-kaur/", role: "Reps" },
+  { name: "Beihao Zhou", link: "https://www.linkedin.com/in/beihaozhou/", role: "Web Dev" },
+  { name: "Cadey Chen", link: "https://linkedin.com/in/cadey-chen/", role: "Design" },
+  { name: "Emily Huang", link: "https://www.linkedin.com/in/emilyhuxng/", role: "Web Dev" },
+  { name: "Franklin Ramirez", link: "https://www.linkedin.com/in/franklin-ramirez611/", role: "Web Dev" },
+  { name: "Hailey Chan", link: "https://linkedin.com/in/chan-hailey/", role: "Design" },
+  { name: "Imane Yacoubi", link: "https://www.linkedin.com/in/iyacoubi", role: "Reps" },
+  { name: "Ivy Lei", link: "https://www.linkedin.com/in/ivy-lei/", role: "Reps" },
+  { name: "Jared He", link: "https://www.linkedin.com/in/jaredhe/", role: "Web Dev" },
+  { name: "Jenny Zhang", link: "https://www.instagram.com/j3nny_zhang", role: "Design" },
+  { name: "Jessica Chan", link: "https://linkedin.com/in/jessica-r-chan/", role: "Design" },
+  { name: "Juthika Hoque", link: "https://www.linkedin.com/in/juthikahoque/", role: "Reps" },
+  { name: "Katherine Liu", link: "https://www.linkedin.com/in/katherine-liu-59b30324a/", role: "Reps" },
+  { name: "Kelly Qu", link: "https://linkedin.com/in/kellyqu/", role: "Design" },
+  { name: "Kevin Cui", link: "https://linkedin.com/in/kevincui1", role: "Reps" },
+  { name: "Kushal Goel", link: "https://www.linkedin.com/in/kushal-goel/", role: "Web Dev" },
+  { name: "Lila Hoang", link: "https://linkedin.com/in/lila-hoang-a13851263/", role: "Design" },
+  { name: "Linda Wang", link: "https://linkedin.com/in/linda-y-wang/", role: "Design" },
+  { name: "Lucy Qi", link: "https://linkedin.com/in/lucyqqi/", role: "Design" },
+  { name: "Mark Chiu", link: "https://linkedin.com/in/markchiu02", role: "Web Dev" },
+  { name: "Max Erenberg", link: "https://maxerenberg.github.io/", role: "Syscom" },
+  { name: "Mayank Mehra", link: "https://linkedin.com/in/mayank808/", role: "Reps" },
+  { name: "Nikki Guo", link: "https://www.linkedin.com/in/nikkiguo", role: "Reps" },
+  { name: "Olivia Liu", link: "#", role: "Reps" },
+  { name: "Rachel Ma", link: "#", role: "Design" },
+  { name: "Raymond Li", link: "https://raymond.li/", role: "Syscom" },
+  { name: "Roni Wu", link: "https://linkedin.com/in/roni-wu-908900140/", role: "Design" },
+  { name: "Ryan Zhou", link: "https://www.linkedin.com/in/ryan-zhou-498b94285/", role: "Web Dev" },
+  { name: "Sat Arora", link: "https://www.linkedin.com/in/sat-arora/", role: "Reps" },
+  { name: "Shahan Nedadahandeh", link: "https://shahan.ca/", role: "Web Dev" },
+  { name: "Vivian Guo", link: "https://www.linkedin.com/in/vivianvg", role: "Design" },
 ];
