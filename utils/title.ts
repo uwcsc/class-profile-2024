00c1e4e5bd0919aabb32392cc7e7e92916651be7
hyperneutrino <hyperneutrino15@gmail.com>
2025-01-27 02:27:44 -0500

    bump year

--- utils/title.ts
+++ utils/title.ts
@@ -1,18 +1,18 @@
 import { Metadata } from "next";
 
 const description =
-  "The 2023 CS Class Profile is a showcase of data relevant to CS, CFM, and CS/BBA students completing their undergrad in 2023. Explore the graduating class of 2023 with data gathered and presented by the University of Waterloo Computer Science Club!";
+  "The 2024 CS Class Profile is a showcase of data relevant to CS, CFM, and CS/BBA students completing their undergrad in 2024. Explore the graduating class of 2024 with data gathered and presented by the University of Waterloo Computer Science Club!";
 
 export function title(title: string): Metadata {
   return {
     title: `${title} — 2024 CS Class Profile`,
     description,
-    keywords: ["uw", "csc", "csclub", "uwaterloo", "uwcsc", "university", "waterloo", "computer", "science", "club", "class", "profile", "2023"],
+    keywords: ["uw", "csc", "csclub", "uwaterloo", "uwcsc", "university", "waterloo", "computer", "science", "club", "class", "profile", "2024"],
     openGraph: {
       type: "website",
-      title: "2023 CS Class Profile",
+      title: "2024 CS Class Profile",
       description,
-      url: "https://csclub.uwaterloo.ca/classprofile/2023/",
+      url: "https://csclub.uwaterloo.ca/classprofile/2024/",
     },
     icons: ["/images/favicon.ico"],
   };
